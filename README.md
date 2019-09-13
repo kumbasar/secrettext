29c123451450a165ecee578c8eac3a131315a706
kumbasar <vkumbasar@outlook.com>
2019-09-14 00:55:21 +0300

    plain string is mandantory arg. Update readme with ussage and example
    Signed-off-by: kumbasar <vkumbasar@outlook.com>

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# secrettext
+# Usage
+
+```bash
+kumbasar@ghost ~/g/secrettext> ./encoder.py --help
+usage: encoder.py [-h] [--shift SHIFT] plain_string
+
+Encoder Help.
+
+positional arguments:
+  plain_string   Input plain text. Example: 'Hello World! 1 2 3'
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --shift SHIFT  set ceaser chiper shift value (default: 7)
+kumbasar@ghost ~/g/secrettext> 
+```
+
+# Example
+
+```bash
+kumbasar@ghost ~/g/secrettext> ./encoder.py --shift 3 'Hello World!! 1 2 9'
+Caesar Shift value: 3
+Plain String: Hello World!! 1 2 9
+Caesar Cipher String: Khoor Zruog!! 1 2 9
+BASE64 String: S2hvb3IgWnJ1b2chISAxIDIgOQ==
+Binary String: 1010011 110010 1101000 1110110 1100010 110011 1001001 1100111 1010111 1101110 1001010 110001 1100010 110010 1100011 1101000 1001001 1010011 1000001 1111000 1001001 1000100 1001001 1100111 1001111 1010001 111101 111101
+kumbasar@ghost ~/g/secrettext> 
+```
--- encoder.py
+++ encoder.py
@@ -6,20 +6,23 @@ import argparse
 
 alphabets = (string.ascii_lowercase, string.ascii_uppercase)
 caesar_shift = 7
-PLAIN_STRING = "Hello World!! 1 2 9"
 
-parser = argparse.ArgumentParser(description='My example explanation')
+parser = argparse.ArgumentParser(description='Encoder Help.')
 parser.add_argument(
     '--shift',
     type=int,
     default=7,
     help='set ceaser chiper shift value (default: ' + str(caesar_shift) + ')'
 )
+parser.add_argument('plain_string', type=str, help='Input plain text. Example: \'Hello World! 1 2 3\'')
+
 encoder_parm = parser.parse_args()
 
 caesar_shift = encoder_parm.shift
+plain_string = encoder_parm.plain_string
 
 print("Caesar Shift value: " + str(encoder_parm.shift))
+print("Plain String: " + plain_string)
 
 
 def caesar(text, step, alphabets):
@@ -33,18 +36,17 @@ def caesar(text, step, alphabets):
     table = string.maketrans(joined_aphabets, joined_shifted_alphabets)
     return text.translate(table)
 
-print("PLAIN_STRING:" + PLAIN_STRING)
 
 # Cesar Cipher
-CAESAR_STRING = caesar( PLAIN_STRING , caesar_shift , alphabets=alphabets)
-print("CAESAR_STRING:" + CAESAR_STRING)
+CAESAR_STRING = caesar( plain_string , caesar_shift , alphabets=alphabets)
+print("Caesar Cipher String: " + CAESAR_STRING)
 
 
 encodedBytes = base64.b64encode(CAESAR_STRING.encode("utf-8"))
 BASE64_STRING = str(encodedBytes)
 
-print("BASE64_STRING:" + BASE64_STRING)
+print("BASE64 String: " + BASE64_STRING)
 
 BINARY_STRING = ' '.join(format(x, 'b') for x in bytearray(BASE64_STRING))
 
-print("BINARY_STRING:" + BINARY_STRING)
+print("Binary String: " + BINARY_STRING)
